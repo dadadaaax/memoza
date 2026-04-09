3a783037bcd528e02b42ca387d08f73e6f27adea
ddd <d@gmail.com>
2026-04-09 12:55:50 +0000

    fix: Enable pinch-to-zoom on mobile devices

--- wp-content/themes/memoza-mobile/index.php
+++ wp-content/themes/memoza-mobile/index.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <?php wp_head(); ?>
     <style>
